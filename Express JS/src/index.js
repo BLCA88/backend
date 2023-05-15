52bc5bf906064958e636125ad78af538d0cfe827
Jose Antunez <jl.antunez88@gmail.com>
2023-05-14 21:22:30 -0300

    Se optimizo el codigo del product manager

--- Express JS/src/index.js
+++ Express JS/src/index.js
@@ -2,16 +2,24 @@ import ProductManager from './product-manager.js';
 
 const generador = new ProductManager('../bdProducts.json');
 
-const env = async () => {
+/* const env = async () => {
     let product = {
         titulo: 'Prueba5',
         descripcion: 'blabla',
         precio: '4000',
         imagen: 'bla1',
-        code: 'AR3'
+        code: 'AR4'
     };
     let result = await generador.addProduct(product);
     console.log(result);
 };
 
-env()
+env() */
+
+const buscarID = async () => {
+    const id = await generador.getProductsById(3);
+    return id;
+}
+
+
+buscarID()
--- Express JS/src/product-manager.js
+++ Express JS/src/product-manager.js
@@ -5,25 +5,61 @@ export default class ProductManager {
         this.path = path;
     };
 
-    async addProduct(productos) {
-        const elementos = await this.getProduct();
+    async addProduct({ titulo, descripcion, precio, imagen, code, stock = 50 }) {
+        const productos = {
+            titulo,
+            descripcion,
+            precio,
+            imagen,
+            code,
+            stock: stock || 50
+        }
+        //Obtiene los elementos del array del archhivo json utilizando el metodo getProducts.
+        const elementos = await this.getProducts();
+
         if (elementos.length === 0) {
             productos.id = 1;
         } else {
             productos.id = elementos[elementos.length - 1].id + 1;
         }
-        elementos.push(productos);
+
+        //CodigoExistente busca dentro de elementos y si encuentra el objeto.code igual a productos.code devuelve true
+        const codigoExistente = elementos.some(p => p.code === productos.code);
+        //CampoVacio verifica dentro del objeto productos si hay un campo undefined y si es asi devuelve true.
+        const campoVacio = Object.values(productos).includes(undefined);
+
+        if (!(codigoExistente || campoVacio)) {
+            elementos.push(productos);
+        } else if (campoVacio) {
+            console.log('Tenes que completar todos los campos para poder crear un producto');
+        } else {
+            console.log(`Ingrese un codigo diferente: ${productos.code} ya existe`);
+        }
+
         await fs.promises.writeFile(this.path, JSON.stringify(elementos, null, '\t'));
         return elementos;
     };
 
-    async getProduct() {
+    async getProducts() {
         try {
             const archivo = await fs.promises.readFile(this.path, 'utf8');
             const productos = JSON.parse(archivo);
             return productos;
         } catch (error) {
+            console.log(error + `Ocurrio un error al interntar leer el archivo con la ruta ${this.path}`);;
             return [];
+        }
+    };
+
+    async getProductsById(id) {
+        const elementos = await this.getProducts();
+        const buscarId = elementos.find(i => i.id === id);
+        console.log(buscarId);
+        if (buscarId) {
+            return buscarId;
+        } else if (buscarId === undefined) {
+            console.log('No existe el producto con el ID solicitado')
+            return;
         }
     };
 
