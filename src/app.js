f7c8a308a581e8e57de09c1d74bd8091e714dace
Jose Antunez <jl.antunez88@gmail.com>
2023-06-25 19:52:52 -0300

    Modificando archivos de rama principal

--- src/app.js
+++ src/app.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import express from 'express';
-import productsRouter from './routes/products.router.js';
-import cartRoutes from './routes/cart.router.js';
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./src/public'));
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartRoutes);
-
-
-app.listen(8080, () => console.log('Escucho loro!'));
-=======
 import express from 'express';
 import SocketManager from './config/socketio/socketio.config.js';
 import displayRoutes from 'express-routemap';
@@ -44,4 +28,3 @@ app.use('/', viewsRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/productsbd', productosbdRouter);
->>>>>>> 208e4fcb9d0d46218156f4a06009327b2661d7a3
